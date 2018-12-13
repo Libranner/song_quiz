4017d4544ddddb0ce363ef7b1f5284fcc29c883b
Clary Morla Gomez <clarymorlagomez@MacBook-Pro-de-Clary.local>
2018-12-13 17:23:53 +0100

    add about page

---
+++ app/src/main/java/es/usj/song_quiz/utilities/AboutActivity.kt
@@ -0,0 +1,30 @@
+package es.usj.song_quiz.utilities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import es.usj.song_quiz.R
+
+class AboutActivity : AppCompatActivity() {
+
+    private lateinit var mContent: View
+    private var shortAnimationDuration: Int = 0
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+
+        mContent = findViewById(R.id.view1)
+        mContent.visibility = View.GONE
+        shortAnimationDuration = resources.getInteger(android.R.integer.config_shortAnimTime)
+
+        mContent.apply {
+           alpha = 0f
+           visibility = View.VISIBLE
+
+          animate().alpha(1f).setDuration(shortAnimationDuration.toLong()).setListener(null)
+        }
+
+        }
+}
